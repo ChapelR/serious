37a032ed45297d23409b0aef83cba1c10a5dff4e
ChapelR <Mason_718@hotmail.com>
2019-01-12 15:44:28 -0500

    split off npm package dev

--- npm/scripts/clean.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-    'use strict';
-
-    var paths = require('./pather.js')();
-    var config = require(paths.cwd + 'config.json');
-    var jetpack = require('fs-jetpack');
-
-    jetpack.remove(config.output + 'content');
-};
